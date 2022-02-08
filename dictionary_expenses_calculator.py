f8d592f80643b7181e4beac7106d48ad9da5fe88
Devam Kalpesh Jani <43711546+DevamKJani@users.noreply.github.com>
2022-02-08 09:57:08 +0530

    Update dictionary_expenses_calculator.py

--- dictionary_expenses_calculator.py
+++ dictionary_expenses_calculator.py
@@ -1,12 +1,12 @@
 import time
 
 total_budget = float(input("Enter your this month's budget (in CAD): "))
-print("Your budget as a whole is $", total_budget)
+print("Your budget as a whole is", total_budget, "$ CAD.")
 
 print("******** Please add your expense ********")
 option = input("Do you want to add an expense, please select: ")
 i = 0
-expense_budget_total = 0
+total_expense = 0
 expense_list = []
 expense_budget_list = []
 
@@ -16,7 +16,7 @@ if (option == 'yes'):
         expense_list.append(expense)
         expense_budget = float(input("Please enter how much was the expense: "))
         expense_budget_list.append(expense_budget)
-        expense_budget_total += expense_budget
+        total_expense += expense_budget
         i =+ 1
         loop_option = input("do you want to add another expense??? ")
         if (loop_option == 'yes'):  
@@ -32,8 +32,13 @@ if (option == 'yes'):
                     break'''  
             # ZIP METHOD TO DICTIONARIZE
             expense_dict_list = dict(zip(expense_list,expense_budget_list))   
-            print ("Expenses: ", expense_dict_list)
-            print("Remaining budget is: $", total_budget-expense_budget_total)
-            break
+            print ("Your expenses: ", expense_dict_list)
+            remaining_budget = total_budget - total_expense
+            if (total_expense > total_budget):
+                print("Its " + str(remaining_budget) + "$." + (" Things just got out of hand!"))
+                break
+            else:
+                print("Remaining budget is:", remaining_budget, "$.")
+                break
 else: 
-    print("Its still $", total_budget, "you cunt. Spend a penny first!!!")
+    print("Its still", total_budget, "$ you cunt. Spend a penny first!!!")
