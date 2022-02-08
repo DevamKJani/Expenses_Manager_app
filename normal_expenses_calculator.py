044d09cf22834975870ae67b2ba2af3c1292bff7
Devam Kalpesh Jani <43711546+DevamKJani@users.noreply.github.com>
2022-02-08 09:58:13 +0530

    Update normal_expenses_calculator.py

--- normal_expenses_calculator.py
+++ normal_expenses_calculator.py
@@ -6,7 +6,7 @@ print("Your budget as a whole is $", total_budget)
 print("******** Please add your expense ********")
 option = input("Do you want to add an expense, please select: ")
 i = 0
-expense_budget_total = 0
+total_expense = 0
 expense_list = []
 if (option == 'yes'):
     while (True):
@@ -14,7 +14,7 @@ if (option == 'yes'):
         expense_list.append(expense)
         expense_budget = float(input("Please enter how much was the expense: "))
         print("The expense of", expense, "is $", expense_budget)
-        expense_budget_total += expense_budget
+        total_expense += expense_budget
         i =+ 1
         loop_option = input("do you want to add another expense??? ")
         if (loop_option == 'yes'):  
@@ -23,13 +23,14 @@ if (option == 'yes'):
         else:
             print("calculating remaining of the total budget...")
             time.sleep(2)
+            print("Your expenses:")
             print(*expense_list, sep = ', ')
-            print("Remaining budget is: $", total_budget-expense_budget_total)
-            break
+            remaining_budget = total_budget - total_expense
+            if (total_expense > total_budget):
+                print("Its " + str(remaining_budget) + "$." + (" Things just got out of hand!"))
+                break
+            else:
+                print("Remaining budget is:", remaining_budget, "$.")
+                break
 else: 
     print("Its still $", total_budget, "you cunt. Spend a penny first!!!")
-
-
-    
-
-
